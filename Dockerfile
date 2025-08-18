b5049532fc98ad95a48cd9e6aab4a2bb8353d7ce
yuvaraju1 <yuvaraju.dasari@talakunchi.com>
2025-08-18 12:27:07 +0000

    Initial commit with vulnerable Flask app

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.6-slim
+
+WORKDIR /app
+COPY . .
+
+RUN pip install -r requirements.txt
+
+EXPOSE 5000
+CMD ["python", "app.py"]
+
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    name = request.args.get("name", "User")
+    template = f"Hello {name}!"
+    return render_template_string(template)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
