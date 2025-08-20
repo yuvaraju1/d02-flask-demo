456fcdc84ab88c898bba1e42bdb1abb2ddc5d9c6
yuvaraju1 <yuvaraju.dasari@talakunchi.com>
2025-08-20 15:10:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string, render_template
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
@@ -7,8 +7,6 @@ def home():
     name = request.args.get("name", "User")
     template = f"Hello {name}!"
     return render_template_string(template)
-    #patch: remove above 2 lines and add below one line, index.html file is recommented
-    #return render_template("index.html", name=name)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
